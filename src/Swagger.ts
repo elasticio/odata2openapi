1ff1c53ee68890373d92be01366ab71a648905a0
Atanas Korchev <akorchev@gmail.com>
2016-07-26 14:08:06 +0300

    Add default export and empty Swagger interface.

---
+++ src/Swagger.ts
@@ -0,0 +1,4 @@
+interface Swagger {
+}
+
+export default Swagger
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Swagger from './Swagger';
+
+function odata2openapi(metadataUrl:string) : Promise<Swagger> {
+  return Promise.resolve(null);
+}
+
+export default odata2openapi;
