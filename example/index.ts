947118d38c95958dfa50dfc2e3c961984f8c2737
Atanas Korchev <akorchev@gmail.com>
2016-07-27 18:13:42 +0300

    Load url from command line.

--- example/index.ts
+++ example/index.ts
@@ -1,5 +1,7 @@
 import { odata2openapi } from '../src/';
 
-odata2openapi('http://services.odata.org/V3/Northwind/Northwind.svc/$metadata')
+const url = process.argv[2] || 'http://services.odata.org/V3/Northwind/Northwind.svc/$metadata';
+
+odata2openapi(url)
   .then(swagger => console.log(JSON.stringify(swagger, null, 2)))
   .catch(error => console.error(error))
