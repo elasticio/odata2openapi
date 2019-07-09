d3d14153fbbb391dd9680a063599f4e2ea65f04d
Emily Marigold Klassen <forivall@gmail.com>
2019-07-09 13:15:54 -0700

    fix: ensure that generated dts is compatible with --strictNullChecks

--- src/Responses.ts
+++ src/Responses.ts
@@ -3,5 +3,5 @@ import { Reference } from './Reference';
 
 export interface Responses {
   default?: Response | Reference;
-  [httpStatusCode: string]: Response | Reference;
+  [httpStatusCode: string]: Response | Reference | undefined;
 }
